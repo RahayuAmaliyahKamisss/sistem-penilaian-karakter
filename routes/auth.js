648958f765b74b7b6a6281dd48a23c83a7998ebf
Rahayu Amaliyah Kamis <xtallmint@gmail.com>
2026-06-21 13:12:03 +0700

    Update proses metode dan perbaikan kuesioner 4.0

--- routes/auth.js
+++ routes/auth.js
@@ -11,12 +11,42 @@ router.post("/login", (req, res) => {
         if (err) throw err
 
         if (result.length > 0) {
-            req.session.user = result[0]
-            res.json({ success: true, user: result[0]})
-        } else {
-            res.json({ success: false })
+
+    req.session.user = result[0];
+
+    console.log("LOGIN SUCCESS");
+    console.log(req.session.user);
+
+    req.session.save((err) => {
+
+        if (err) {
+            console.log("SESSION ERROR:", err);
+
+            return res.status(500).json({
+                success: false,
+                message: err.message
+            });
         }
+
+        console.log("SESSION SAVED");
+        console.log(req.session);
+
+        res.json({
+            success: true,
+            user: result[0]
+        });
+    });
+
+} else {
+
+    res.json({
+        success: false
+    });
+
+}
     })
 })
-
+router.get("/cek-session", (req, res) => {
+    res.json(req.session);
+});
 module.exports = router
--- server.js
+++ server.js
@@ -7,14 +7,22 @@ const app = express();
 // =====================
 // MIDDLEWARE
 // =====================
-app.use(cors());
+app.use(cors({
+    origin: true,
+    credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(session({
     secret: "secretkey",
     resave: false,
-    saveUninitialized: true
+    saveUninitialized: false,
+    cookie: {
+        httpOnly: true,
+        secure: false,
+        maxAge: 24 * 60 * 60 * 1000
+    }
 }));
 
 // Static file
